f900a9672a97fd92c2607453135b0ae5ffe51d4a
Akshay Kumar <Akshay@Akshay-Mac-mini.local>
2019-05-09 17:16:45 +0530

    Game class will hold my bussiness logic

---
+++ Assignment/Model/GameLogic.swift
@@ -0,0 +1,20 @@
+//
+//  GameLogic.swift
+//  Assignment
+//
+//  Created by Akshay Kumar on 09/05/19.
+//  Copyright © 2019 Akshay Kumar. All rights reserved.
+//
+
+import Foundation
+class Game {
+    let width: Int
+    let height: Int
+   
+    
+    init(width: Int, height: Int) {
+        self.width = width
+        self.height = height
+    }
+    
+}

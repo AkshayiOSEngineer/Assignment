f665a79be68cb211b93950d16d8d4ab5efd00b6c
Akshay Kumar <Akshay@Akshay-Mac-mini.local>
2019-05-09 17:22:11 +0530

    Add Track and Cell modal class

---
+++ Assignment/Model/CellModel.swift
@@ -0,0 +1,14 @@
+//
+//  CellModel.swift
+//  Assignment
+//
+//  Created by Akshay Kumar on 09/05/19.
+//  Copyright © 2019 Akshay Kumar. All rights reserved.
+//
+
+import Foundation
+import Foundation
+
+struct Cell {
+   
+}
